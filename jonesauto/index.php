25f6213926b871e925df394d73375910f3b1966f
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2026-04-07 20:47:13 -0600

    fix style

--- jonesauto/index.php
+++ jonesauto/index.php
@@ -16,23 +16,28 @@
         <div class="section-title">
             <h2>Form Actions</h2>
         </div>
+
         <div class="cards">
             <div class="card">
                 <h3>Add Vehicle</h3>
                 <a href="input forms/add_vehicle.php">Open Form</a>
             </div>
+
             <div class="card">
                 <h3>Record Purchase</h3>
                 <a href="input forms/purchase_vehicle.php">Open Form</a>
             </div>
+
             <div class="card">
                 <h3>Record Sale</h3>
                 <a href="input forms/sale_form.php">Open Form</a>
             </div>
+
             <div class="card">
                 <h3>Record Payment</h3>
                 <a href="input forms/payment_form.php">Open Form</a>
             </div>
+
             <div class="card">
                 <h3>Record Warranty Sale</h3>
                 <a href="input forms/warranty_form.php">Open Form</a>
@@ -42,6 +47,7 @@
         <div class="section-title">
             <h2>Reports</h2>
         </div>
+
         <div class="report-links">
             <a href="reports/view_vehicles.php">View Vehicles</a>
             <a href="reports/inventory_report.php">Inventory Report</a>
